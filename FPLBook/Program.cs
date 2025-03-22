dcaea7ba95de1ebb562433a08d5d827acc9874a2
gachienmam <thanhdatcaodang@gmail.com>
2025-03-22 08:34:48 +0700

    CLI: Chức năng mẫu

--- FPLBook/Program.cs
+++ FPLBook/Program.cs
@@ -1,10 +1,25 @@
 ﻿using System.Runtime.CompilerServices;
+using System.CommandLine;
 
 internal class Program
 {
     private static async Task<int> Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        return 0;
+        // --inputFile: Lấy vị trí file và đọc để xử lý
+        var fileOption = new Option<FileInfo?>(
+            name: "--inputFile",
+            description: "Lấy vị trí file và đọc để xử lý.");
+
+        var rootCommand = new RootCommand("CLI xử lý file");
+        rootCommand.AddOption(fileOption);
+
+        rootCommand.SetHandler((file) =>
+        {
+            
+            //ReadFile(file!);
+        },
+            fileOption);
+
+        return await rootCommand.InvokeAsync(args);
     }
 }
