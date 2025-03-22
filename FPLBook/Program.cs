2603ad3817e8da2f910fe5ac6803bdf55a1d9e67
gachienmam <thanhdatcaodang@gmail.com>
2025-03-22 09:34:34 +0700

    Hoàn thiện CLI

--- FPLBook/Program.cs
+++ FPLBook/Program.cs
@@ -5,6 +5,8 @@ internal class Program
 {
     private static async Task<int> Main(string[] args)
     {
+        Console.InputEncoding = System.Text.Encoding.UTF8;
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
         /*
          * Vị trí thêm option cho lệnh
          */
@@ -28,7 +30,7 @@ internal class Program
             name: "--sortColumn",
             description: "Tên cột để xếp theo thứ tự (tách nhau bởi dấu phẩy (,))");
 
-        // 4: Tìm kiếm sách theo từ khóa
+        // 4: Tìm kiếm sách theo từ khóa (lọc từ khóa)
         var searchKeywordOption = new Option<string[]>(
             name: "--searchKeyword",
             description: "Từ khóa tìm kiếm");
@@ -37,8 +39,10 @@ internal class Program
             description: "Tên cột để tìm kiếm (tách nhau bởi dấu phẩy (,))");
 
         // 5: Thống kê số lượng sách và chủ đề theo nhà xuất bản
+        // Không có flag
 
-
+        // 6: Xử lý sách trùng lặp
+        // Không có flag
 
         // 7, 8
         var encryptionKeyOption = new Option<string>(
@@ -56,13 +60,15 @@ internal class Program
          * Vị trí thêm lệnh
          */
 
-        // 1
+        // 2
         var indexCommand = new Command("index", "Tạo cột index cho tệp tin sau cột được cho.")
             {
                 inputFileOption,
                 indexAfterOption,
                 outputFileOption,
             };
+
+        // 3
         var sortCommand = new Command("sort", "Sắp xếp tệp tin theo cột được cho.")
             {
                 inputFileOption,
@@ -70,28 +76,107 @@ internal class Program
                 outputFileOption,
             };
 
+        // 4
+        var searchCommand = new Command("search", "Tìm kiếm tệp tin theo cột được cho.")
+            {
+                inputFileOption,
+                searchKeywordOption,
+                searchColumnOption,
+                outputFileOption,
+            };
+
+        // 5
+        var analyticsCommand = new Command("analytics", "Thống kê tệp tin theo cột được cho.")
+            {
+                inputFileOption,
+                outputFileOption,
+            };
+
+        // 6
+        var duplicateCommand = new Command("duplicate", "Lọc dữ liệu bị trùng trong tệp tin theo cột được cho và xuất ra tệp tin.")
+            {
+                inputFileOption,
+                outputFileOption,
+            };
+
+        // 7
         var encryptCommand = new Command("encrypt", "Mã hóa tệp tin.")
             {
                 inputFileOption,
                 encryptionKeyOption,
                 outputFileOption,
             };
+
+        // 8
         var decryptCommand = new Command("decrypt", "Giải mã hóa tệp tin.")
             {
                 inputFileOption,
                 encryptionKeyOption,
                 outputFileOption,
             };
-        rootCommand.AddCommand(indexCommand);
-        rootCommand.AddCommand(sortCommand);
-        rootCommand.AddCommand(encryptCommand);
-        rootCommand.AddCommand(decryptCommand);
 
+        // Thêm lệnh vào root
+        rootCommand.AddCommand(indexCommand); // 2
+        rootCommand.AddCommand(sortCommand); // 3
+        rootCommand.AddCommand(searchCommand); // 4
+        rootCommand.AddCommand(analyticsCommand); // 5
+        rootCommand.AddCommand(duplicateCommand); // 6
+        rootCommand.AddCommand(encryptCommand); // 7
+        rootCommand.AddCommand(decryptCommand); // 8
+        
+        /*
+         * Thêm phần xử lý cho lệnh
+         */
+
+        // 2
         indexCommand.SetHandler(async (input, indexAfter, output) =>
         {
             // Cho lệnh ở dưới
             //await 
         }, inputFileOption, indexAfterOption, outputFileOption);
+
+        // 3
+        sortCommand.SetHandler(async (input, sortColumn, output) =>
+        {
+            // Cho lệnh ở dưới
+            //await 
+        }, inputFileOption, sortColumnOption, outputFileOption);
+
+        // 4
+        searchCommand.SetHandler(async (input, searchKeyword, searchColumn, output) =>
+        {
+            // Cho lệnh ở dưới
+            //await 
+        }, inputFileOption, searchKeywordOption, searchColumnOption, outputFileOption);
+
+        // 5
+        analyticsCommand.SetHandler(async (input, output) =>
+        {
+            // Cho lệnh ở dưới
+            //await 
+        }, inputFileOption, outputFileOption);
+
+        // 6
+        duplicateCommand.SetHandler(async (input, output) =>
+        {
+            // Cho lệnh ở dưới
+            //await 
+        }, inputFileOption, outputFileOption);
+
+        // 7
+        encryptCommand.SetHandler(async (input, encryptionKey, output) =>
+        {
+            // Cho lệnh ở dưới
+            //await 
+        }, inputFileOption, encryptionKeyOption, outputFileOption);
+
+        // 8
+        decryptCommand.SetHandler(async (input, decryptionKey, output) =>
+        {
+            // Cho lệnh ở dưới
+            //await 
+        }, inputFileOption, decryptionKeyOption, outputFileOption);
+
 
         return await rootCommand.InvokeAsync(args);
     }
