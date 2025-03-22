c441b834379bd2b6820ae44980fd02e4dac2cc02
PhhHuynn <phuonghuyen5872@gmail.com>
2025-03-22 09:45:44 +0700

    sửa hàm timkiem, đảm báo dùng được cho nhiều kiểu file khác

--- FPLBook/Modules/TimKiem.cs
+++ FPLBook/Modules/TimKiem.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,15 +10,22 @@ namespace FPLBook.Modules
     public static class TimKiem
     {
 
-        public static void TimKiemTheoTieuDe(string keyword, string filePath) 
+        public static void TimKiemTheoTieuDe(string keyword, string filePath)
         {
             List<string[]> books = new List<string[]>();
-
+            string[] headers;
+            
+            int titleIndex = -1;
             using (var reader = new StreamReader(filePath))
             {
-                string headerLine = reader.ReadLine();
-                string[] headers = headerLine.Split(',');
+                headers = ParseCsvLine(reader.ReadLine());
+                titleIndex = Array.FindIndex(headers, h => h.Trim().ToLower() == "title");
 
+                if (titleIndex == -1)
+                {
+                    Console.WriteLine("❌ Không tìm thấy cột 'Title' trong file!");
+                    return;
+                }
                 while (!reader.EndOfStream)
                 {
                     string line = reader.ReadLine();
@@ -33,20 +41,18 @@ namespace FPLBook.Modules
             }
 
             keyword = keyword.Trim().ToLower();
-
-            var searchResults = books.Where(b => b[0].ToLower().Contains(keyword)).ToList();
+            var searchResults = books.Where(b => b[titleIndex].ToLower().Contains(keyword)).ToList();
 
             if (searchResults.Count == 0)
             {
-                Console.WriteLine("❌ Không tìm thấy sách nào!");
+                Console.WriteLine("Không tìm thấy sách nào!");
             }
             else
             {
-                PrintTable(searchResults);
+                PrintTable(headers, searchResults);
             }
         }
 
-        // Xử lý dòng CSV chứa dấu ngoặc kép (dùng cho những ô có dấu phẩy)
         static string[] ParseCsvLine(string line)
         {
             List<string> result = new List<string>();
@@ -56,44 +62,40 @@ namespace FPLBook.Modules
             foreach (char c in line)
             {
                 if (c == '"' && inQuotes)
-                {
                     inQuotes = false;
-                }
                 else if (c == '"')
-                {
-                    inQuotes = true; 
-                }
+                    inQuotes = true;
                 else if (c == ',' && !inQuotes)
                 {
                     result.Add(current.Trim());
                     current = "";
                 }
                 else
-                {
                     current += c;
-                }
             }
             result.Add(current.Trim());
 
             return result.ToArray();
         }
 
-        static void PrintTable(List<string[]> books)
+        static void PrintTable(string[] headers, List<string[]> books)
         {
-            int[] columnWidths = { 40, 30, 20, 10, 25 };
+            int columnCount = headers.Length;
+            int[] columnWidths = new int[columnCount];
 
-            string header = string.Format(
-                "| {0,-" + columnWidths[0] + "} | {1,-" + columnWidths[1] + "} | {2,-" + columnWidths[2] + "} | {3,-" + columnWidths[3] + "} | {4,-" + columnWidths[4] + "} |",
-                "Title", "Author", "Genre", "Height", "Publisher");
+            for (int i = 0; i < columnCount; i++)
+            {
+                columnWidths[i] = Math.Max(headers[i].Length, books.Max(b => b[i].Length)) + 2;
+            }
 
-            Console.WriteLine(header);
-            Console.WriteLine(new string('-', header.Length));
+            string headerRow = "| " + string.Join(" | ", headers.Select((h, i) => h.PadRight(columnWidths[i]))) + " |";
+            Console.WriteLine(headerRow);
+            Console.WriteLine(new string('-', headerRow.Length));
 
             foreach (var book in books)
             {
-                Console.WriteLine(string.Format(
-                    "| {0,-" + columnWidths[0] + "} | {1,-" + columnWidths[1] + "} | {2,-" + columnWidths[2] + "} | {3,-" + columnWidths[3] + "} | {4,-" + columnWidths[4] + "} |",
-                    book[0], book[1], book[2], book[3], book[4]));
+                string row = "| " + string.Join(" | ", book.Select((b, i) => b.PadRight(columnWidths[i]))) + " |";
+                Console.WriteLine(row);
             }
         }
 
